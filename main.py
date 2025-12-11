9706494705a6edb699d15aad607a84451d9165ba
mikapau01234 <benpau01234@gmail.com>
2025-12-11 01:04:09 +0100

    increased poslogs cycle amount to 100k

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ def positionStep(p):
     return(p)
 
 def saveStep(p,count):
-    if count % 1000 == True:
+    if count % 100000 == True:
         for planet in p:
             saveLocation = os.path.join(mainDir, posHistoryPath,str(planet.id),str(count))
             print(f"saved to: {saveLocation}")
